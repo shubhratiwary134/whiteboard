b1ac24e8a8e8920466062e71f04549af31b4003f
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-05 17:31:56 +0530

    styling of comment added

--- live-board/src/components/Comment.jsx
+++ live-board/src/components/Comment.jsx
@@ -50,11 +50,12 @@ export default function Comment({threadId,x,y}){
         <>
         <div style={{width:'100px',height:'100px',top:`${y}px`,left:`${x}px`,position:'absolute'}}
         onPointerDown={handlePointerDown}
+        className='flex flex-col items-start'
         >
-          <button className='rounded w-10 bg-gray-200' onClick={handleDissolve}  ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M491-200q12-1 20.5-9.5T520-230q0-14-9-22.5t-23-7.5q-41 3-87-22.5T343-375q-2-11-10.5-18t-19.5-7q-14 0-23 10.5t-6 24.5q17 91 80 130t127 35ZM480-80q-137 0-228.5-94T160-408q0-100 79.5-217.5T480-880q161 137 240.5 254.5T800-408q0 140-91.5 234T480-80Zm0-80q104 0 172-70.5T720-408q0-73-60.5-165T480-774Q361-665 300.5-573T240-408q0 107 68 177.5T480-160Zm0-320Z"/></svg></button>
-          {dissolve && <div >
-            <textarea  onChange={handleInputChange} value={localCommentValue}></textarea>
-           <button onClick={handleSubmit} className='bg-white py-1 px-2'>+</button>
+          <button className='rounded w-10 bg-black-200' onClick={handleDissolve}  ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M880-80 720-240H160q-33 0-56.5-23.5T80-320v-480q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v720ZM160-320h594l46 45v-525H160v480Zm0 0v-480 480Z"/></svg></button>
+          {dissolve && <div className='mt-2 flex flex-col items-center' >
+            <textarea  onChange={handleInputChange} value={localCommentValue} placeholder='Enter Comment' className='bg-black p-2 rounded-xl  text-white mb-4 focus:outline-none'></textarea>
+           <button onClick={handleSubmit} className=' text-white border-4 border-black text-sm  p-3 rounded-lg  bg-transparent hover:bg-white hover:text-black hover:scale-110 dark:text-neutral-200 transition duration-500'>Add Comment</button>
           </div>
           }
         
