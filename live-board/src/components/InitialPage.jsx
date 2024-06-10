2b74ac3ecf3646b574f1f638093488a5385bbc9d
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-10 15:22:43 +0530

    changes in responsiveness

--- live-board/src/components/InitialPage.jsx
+++ live-board/src/components/InitialPage.jsx
@@ -46,23 +46,28 @@ else{
         
         <>
         <Navbar></Navbar>
-        <div className=' text-black flex justify-around items-center'>
-            <div className='hidden md:flex'>
+        <div className=' text-black flex justify-around items-center h-3/4'>
+            <div className='hidden sm:flex'>
                 <img src={logo}></img>
             </div>
-            <div className='flex flex-col justify-between '>
-            <button onClick={EnterRooms} className='shadow-[inset_0_0_0_2px_#616467] text-white px-12 py-4 mb-10 md:px-0 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200  '>Create Room</button>
-          <div>
+            <div className='flex flex-col md:flex-row w-1/2 justify-around '>
+            <div>
+            <button onClick={EnterRooms} className='shadow-[inset_0_0_0_2px_#616467] text-white px-10 py-2 mb-5 lg:px-12 lg:py-4 lg:mb-10  rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200  '>Create Room</button>
+            
+            </div>
+          <div >
           <button onClick={()=>{setJoin(!join)
-            }} className='  shadow-[inset_0_0_0_2px_#616467] text-white px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200 flex justify-around'>Join Room 
+            }} className='  shadow-[inset_0_0_0_2px_#616467] text-white  px-10 py-2 mb-5 lg:px-12 lg:py-4 lg:mb-10 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200 flex justify-around items-center'>Join Room 
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg>
-            </button></div>
-            {join ?<div className='flex flex-col p-2'><input className='p-1 my-1 bg-black text-white border-b-2 focus:outline-none ' type='text' placeholder='Enter The RoomID'
+            </button>
+            {join ?<div className='flex flex-col p-1'><input className='p-1 my-1 bg-black text-white border-b-2 focus:outline-none ' type='text' placeholder='Enter The RoomID'
             onChange={(e)=>setTempRoomId(e.target.value)}
              value={tempRoomId}></input>
-              <button onClick={joinRoom} className='shadow-[inset_0_0_0_2px_#616467] text-white my-4  py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200 '>submit</button>
+              <button onClick={joinRoom} className='shadow-[inset_0_0_0_2px_#616467] text-white my-4  py-2 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200 '>submit</button>
              </div>:null}
             </div>
+          </div>
+          
          
             
              
